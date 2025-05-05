a8bb3d786166c2f561fd5b97fd0e67bbc990b250
Gaddini <ferreiragaddini@bol.com.br>
2025-05-05 10:30:08 -0300

    [Agendamentos] - Ajuste nas classes de usuário

--- MedSched/src/main/java/com/fiap/medsched/dtos/CreateUpdateUserRequest.java
+++ MedSched/src/main/java/com/fiap/medsched/dtos/CreateUpdateUserRequest.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import com.fiap.medsched.enums.UserType;
-
 @Getter
 @Setter
 @AllArgsConstructor
--- MedSched/src/main/java/com/fiap/medsched/entities/Users.java
+++ MedSched/src/main/java/com/fiap/medsched/entities/Users.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(nullable = false)
     private String name;
