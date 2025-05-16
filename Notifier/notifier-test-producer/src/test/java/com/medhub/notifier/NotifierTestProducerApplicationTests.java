4e421fb537992bdb9e834282db53e6ccb0de3862
lizfcordeiro <lizcordeiro@outlook.com>
2025-05-16 00:11:53 -0300

    [docker-create] creating docker configurations with test applications

--- Notifier/notifier-test-producer/src/test/java/com/medhub/notifier/NotifierTestProducerApplicationTests.java
+++ Notifier/notifier-test-producer/src/test/java/com/medhub/notifier/NotifierTestProducerApplicationTests.java
@@ -2,8 +2,10 @@ package com.medhub.notifier;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.kafka.bootstrap-servers=localhost:9093")
 class NotifierTestProducerApplicationTests {
 
 	@Test
