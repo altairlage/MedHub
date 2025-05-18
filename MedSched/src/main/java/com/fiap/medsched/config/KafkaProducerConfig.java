cbcb4a55f805152beca92a8c2c1b8bf0c827132a
lizfcordeiro <lizcordeiro@outlook.com>
2025-05-18 16:40:45 -0300

    [docker-create] Correção do tópico

--- MedSched/src/main/java/com/fiap/medsched/config/KafkaProducerConfig.java
+++ MedSched/src/main/java/com/fiap/medsched/config/KafkaProducerConfig.java
@@ -21,7 +21,7 @@ public class KafkaProducerConfig {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-1:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 10_000);
---
+++ Notifier/notifier/src/main/java/com/medhub/notifier/configuration/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.medhub.notifier.configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic("schedule-notification", 1, (short) 1);
+    }
+}
